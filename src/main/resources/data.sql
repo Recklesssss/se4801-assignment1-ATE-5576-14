06a0272d7055c385e1505ef270b5d737f484a8fe
Recklesssss <biruktesfa444@gmail.com>
2026-04-03 18:25:06 +0300

    Add API testing files and data seeder

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO categories (name, description) VALUES ('Electronics', 'Electronic devices and gadgets');
+INSERT INTO categories (name, description) VALUES ('Books', 'Physical and digital books');
